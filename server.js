5e71b25d73a4d7a6e6f465399c9b08a56e9f21fc
theWellHopeErr <ssuryarajan@gmail.com>
2020-02-03 10:07:52 +0530

    Ready for hosting

--- server.js
+++ server.js
@@ -22,6 +22,15 @@ app.use("/api", require("./api"));
 
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 server.listen(port, () => {
   console.log(`Running in ${port}`);
 });
